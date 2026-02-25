bbec9958c7b189e02fd58f68f271ec714a547e9f
Muhammad hashim <mh4165209@gmail.com>
2026-02-25 10:10:19 -0800

    craet a navbar and banner section complete and also navbar and banner section code push

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const toggle = document.getElementById("menu-toggle");
+    const nav = document.getElementById("nav-links");
+
+    toggle.addEventListener("click", function(){
+        nav.classList.toggle("active");
+    });
+
+});
+
+
 document.documentElement.style.setProperty('--animate-duration', '2s');
 
 // All animations will take half the time to accomplish
