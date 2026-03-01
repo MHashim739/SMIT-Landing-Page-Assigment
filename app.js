f90c1ce9166bb38617b8bea29b7e756e54e7022a
Muhammad hashim <mh4165209@gmail.com>
2026-02-28 23:09:15 -0800

    menu burger javascript clean

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-<script>
+
     const toggle = document.getElementById("menu-toggle");
     const navLinks = document.getElementById("nav-links");
 
     toggle.addEventListener("click", function () {
         navLinks.classList.toggle("active");
     });
-</script>
